3c1ed2139f32e9afac2cb74176c94d6cf18ea4a9
Macbook <alex@alexs-MacBook-Pro-2.local>
2024-12-12 04:09:27 +0200

    analytics2

--- src/components/MicrosoftClarity.tsx
+++ src/components/MicrosoftClarity.tsx
@@ -4,14 +4,22 @@ import Script from 'next/script';
 
 export default function MicrosoftClarity() {
   return (
-    <Script id="microsoft-clarity" strategy="afterInteractive">
-      {`
-        (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "pclebv1xx3");
-      `}
-    </Script>
+    <>
+      <Script
+        id="microsoft-clarity-script"
+        strategy="afterInteractive"
+        src={`https://www.clarity.ms/tag/pclebv1xx3`}
+      />
+      <Script
+        id="microsoft-clarity-init"
+        strategy="afterInteractive"
+      >
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          })(window, document, "clarity", "script", "pclebv1xx3");
+        `}
+      </Script>
+    </>
   );
 } 
